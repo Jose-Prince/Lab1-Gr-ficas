522fda6f9617caf9f42ff244d0fcdd5d3682379f
Jose-Prince <joseprince148@gmail.com>
2024-07-11 16:15:51 -0600

    Feat: Poligon Drawn

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,41 @@
-mod color;
 mod framebuffer;
-mod bmp;
 mod line;
 mod polygon;
+mod color;
+mod bmp;
 
 use framebuffer::Framebuffer;
-use color::Color;
 use line::Line;
 use polygon::Polygon;
+use color::Color;
 use nalgebra_glm as glm;
 
 fn main() {
-    let width = 800;
-    let height = 800;
-    let mut fb = Framebuffer::new(width, height);
+    let mut framebuffer = Framebuffer::new(800, 600); // Crea un Framebuffer de 800x600
 
+    // Define un array de puntos para un polígono
     let points: Vec<[isize; 2]> = vec![
-        [100, 100],
-        [400, 500],
-        [700, 300],
+        [165, 380],
+        [185, 360],
+        [180, 330],
+        [207, 345],
+        [233, 330],
+        [230, 360],
+        [250, 380],
+        [220, 385],
+        [205, 410],
+        [193, 383],
     ];
 
     // Define los colores para el borde y el relleno
-    let border_color = Color::new(255, 0, 0); // Rojo
-    let fill_color = Color::new(0, 255, 0);   // Verde
-    
-    fb.polygon(&points, border_color, fill_color);
+    let border_color = Color::new(255, 255, 255); // Rojo
+    let fill_color = Color::new(255, 255, 0);   // Verde
 
-    // Save the framebuffer to a BMP file
-    if let Err(e) = fb.save_as_bmp("vertex.bmp") {
-        eprintln!("Failed to write BMP file: {}", e);
+    // Dibuja el polígono usando el framebuffer
+    framebuffer.polygon(&points, border_color, fill_color);
+
+    // Guarda el framebuffer como un archivo BMP
+    if let Err(e) = framebuffer.save_as_bmp("out.bmp") {
+        eprintln!("Error al guardar el archivo BMP: {}", e);
     }
-
 }
--- target/debug/deps/main-a5e6608f906a9798.d
+++ target/debug/deps/main-a5e6608f906a9798.d
@@ -1,10 +1,10 @@
-/home/akice/Documentos/Repositories/Drawer/main/target/debug/deps/main-a5e6608f906a9798: src/main.rs src/color.rs src/framebuffer.rs src/bmp.rs src/line.rs src/polygon.rs
+/home/akice/Documentos/Repositories/Lab1-Gr-ficas/target/debug/deps/main-a5e6608f906a9798: src/main.rs src/framebuffer.rs src/line.rs src/polygon.rs src/color.rs src/bmp.rs
 
-/home/akice/Documentos/Repositories/Drawer/main/target/debug/deps/main-a5e6608f906a9798.d: src/main.rs src/color.rs src/framebuffer.rs src/bmp.rs src/line.rs src/polygon.rs
+/home/akice/Documentos/Repositories/Lab1-Gr-ficas/target/debug/deps/main-a5e6608f906a9798.d: src/main.rs src/framebuffer.rs src/line.rs src/polygon.rs src/color.rs src/bmp.rs
 
 src/main.rs:
-src/color.rs:
 src/framebuffer.rs:
-src/bmp.rs:
 src/line.rs:
 src/polygon.rs:
+src/color.rs:
+src/bmp.rs:
