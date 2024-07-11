bd2defddd97c0ea8118ab820e2b4eddbecfff800
Jose-Prince <joseprince148@gmail.com>
2024-07-11 16:23:24 -0600

    Feat: Polygon 2 Drawn

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn main() {
     let mut framebuffer = Framebuffer::new(800, 600); // Crea un Framebuffer de 800x600
 
     // Define un array de puntos para un polígono
-    let points: Vec<[isize; 2]> = vec![
+    let mut points: Vec<[isize; 2]> = vec![
         [165, 380],
         [185, 360],
         [180, 330],
@@ -28,11 +28,21 @@ fn main() {
     ];
 
     // Define los colores para el borde y el relleno
-    let border_color = Color::new(255, 255, 255); // Rojo
-    let fill_color = Color::new(255, 255, 0);   // Verde
+    let mut border_color = Color::new(255, 255, 255); 
+    let mut fill_color = Color::new(255, 255, 0);   
 
     // Dibuja el polígono usando el framebuffer
     framebuffer.polygon(&points, border_color, fill_color);
+
+    points = vec![
+        [321, 335], [288, 286], [339, 251], [374, 302]
+    ];
+
+    border_color = Color::new(255, 255, 255); 
+    fill_color = Color::new(0, 0, 255); 
+
+    framebuffer.polygon(&points, border_color, fill_color);
+
 
     // Guarda el framebuffer como un archivo BMP
     if let Err(e) = framebuffer.save_as_bmp("out.bmp") {
