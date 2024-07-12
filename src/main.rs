be9f8bffffae78653842e9423ca27d5bb55dbd13
Jose-Prince <joseprince148@gmail.com>
2024-07-11 18:47:38 -0600

    Feat: Last Poligon

--- src/main.rs
+++ src/main.rs
@@ -43,6 +43,34 @@ fn main() {
 
     framebuffer.polygon(&points, border_color, fill_color);
 
+    points = vec![
+        [377, 249], [411, 197], [436, 249]
+    ];
+
+    border_color = Color::new(255, 255, 255); 
+    fill_color = Color::new(255, 0, 0); 
+
+    framebuffer.polygon(&points, border_color, fill_color);
+
+    points = vec![
+        [413, 177], [448, 159], [502, 88], [553, 53], [535, 36], [676, 37], [660, 52],
+        [750, 145], [761, 179], [672, 192], [659, 214], [615, 214], [632, 230], [580, 230],
+        [597, 215], [552, 214], [517, 144], [466, 180]
+    ];
+
+    border_color = Color::new(255, 255, 255); 
+    fill_color = Color::new(0, 255, 0); 
+
+    framebuffer.polygon(&points, border_color, fill_color);
+
+    points = vec![
+        [682, 175], [708, 120], [735, 148], [739, 170]
+    ];
+
+    border_color = Color::new(255, 255, 255);
+    fill_color = Color::new(0, 0, 0); // Puedes cambiar el color de relleno si lo deseas
+
+    framebuffer.polygon(&points, border_color, fill_color);
 
     // Guarda el framebuffer como un archivo BMP
     if let Err(e) = framebuffer.save_as_bmp("out.bmp") {
